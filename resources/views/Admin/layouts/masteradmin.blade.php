7e623e475397ecafa7f71eac15b4a6e31873959e
anjalidb345 <anjali@digitalbower.com>
2025-03-20 15:25:31 +0530

    Updated layouts

--- resources/views/Admin/layouts/masteradmin.blade.php
+++ resources/views/Admin/layouts/masteradmin.blade.php
@@ -2,9 +2,9 @@
 <html lang="en">
     <head>
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <title>Kaiadmin - Bootstrap 5 Admin Dashboard</title>
+        <title>@yield('title', 'Admin Dashboard')</title>
         <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport"/>
-        <link rel="icon" href="assets/img/kaiadmin/favicon.ico"  type="image/x-icon"/>
+        <link rel="icon" href="{{asset('assets/img/kaiadmin/favicon.ico')}}"  type="image/x-icon"/>
         @include('admin.layouts.headercss')
     </head>
     <body>
