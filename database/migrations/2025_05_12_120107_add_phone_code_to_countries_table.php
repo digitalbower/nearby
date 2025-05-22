826bd2619f7bc5e659ad4312c67c1f50d64fa94d
anilasajikumar <86340025+anilasajikumar@users.noreply.github.com>
2025-05-22 17:53:08 +0530

    changes

--- database/migrations/2025_05_12_120107_add_phone_code_to_countries_table.php
+++ database/migrations/2025_05_12_120107_add_phone_code_to_countries_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('countries', function (Blueprint $table) {
-            $table->string('phone_code', 10)->after('country_code')->nullable();
+            $table->string('phone_code', 10)->after('iso')->nullable();
         });
     }
 
