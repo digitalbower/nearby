f0b6064c9d0a1a9cc35916affe46b6503c8b5d31
anilasajikumar <86340025+anilasajikumar@users.noreply.github.com>
2025-06-03 19:19:53 +0530

    error

---
+++ resources/views/user/errors/custom.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Page Not Found</title>
+</head>
+<body>
+    <h1>Oops! 404 - Page Not Found</h1>
+    <p>The page you’re looking for doesn’t exist or has been moved.</p>
+    <a href="{{ url('/') }}">Return to Home</a>
+</body>
+</html>
